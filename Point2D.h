fb0a942d4ce5822c7c5b76e3e77a8ff3548bb5a5
kazz <gang9696@gmail.com>
2023-07-25 17:56:30 +0900

    연산자 오버로딩 Plus

--- Point2D.h
+++ Point2D.h
@@ -10,19 +10,71 @@ private:
 public:
 	Point2D() : Point2D(0, 0) // 대리 생성자
 	{
-		std::cout << "<기본 생성자>" << std::endl;
+		
 	}
 
 	Point2D(int x, int y) : mX(x), mY(y)
 	{
-		std::cout << "<기본 생성자>" << std::endl;
+	
 	}
-	~Point2D();
+	~Point2D(){}
 
 	void Distance() const;
 	void Print() const;
 	void Input(int x, int y);
+
+	friend Point2D operator + (Point2D arg1, Point2D arg2);
+
+	Point2D operator - (const Point2D& op)
+	{
+		return Point2D(mX - op.mX, mY - op.mY);
+	}
+
+	/// ---------- ++ 연산자 오버로딩
+	friend const Point2D& operator ++ (Point2D& arg1)
+	{
+		arg1.mX++;
+		arg1.mY++;
+
+		return arg1;
+	}
+
+	// ----------- -- 연산자 오버로딩
+	 Point2D& operator -- ()
+	{
+		mX--;
+		mY--;
+
+		return *this;
+	}
+
+	 // ----------- 후위 연산자: 전역 친구 함수
+	 friend const Point2D& operator++ (Point2D& arg1, int v) // 뒤에는 아무 매개변수나 추가;
+	 {
+		 //대입 후
+		 Point2D temp(arg1.mX, arg1.mY);
+
+		 // 증가
+		 arg1.mX++;
+		 arg1.mY++;
+
+		 return temp;
+	 }
+
+	 // ----------- 후위 연산자 : 멤버 함수
+	 Point2D operator -- (int v)
+	 {
+		 // 대입 후
+		 Point2D temp(mX, mY);
+
+		 // 감소
+		 mX--;
+		 mY--;
+
+		 return temp;
+	 }
 };
+
 
 void Point2D::Print() const
 {
@@ -38,4 +90,14 @@ void Point2D::Input(int x, int y)
 {
 	mX = x;
 	mY = y;
+}
+
+Point2D operator + (Point2D arg1, Point2D arg2)
+{
+	Point2D result;
+
+	result.mX = arg1.mX + arg2.mX;
+	result.mY = arg1.mY + arg2.mY;
+
+    return result;
 }
--- main.cpp
+++ main.cpp
@@ -83,6 +83,8 @@ int main()
 }
 
 */
+
+/*
 class Sword; // 전방 선언
 
 class Player
@@ -125,4 +127,41 @@ int main()
 
 	p1.AttackWith(muramasa);
 	Upgrade(muramasa);
+}
+
+*/
+
+class MyInt
+{
+public:
+	int mValue;
+
+	MyInt() : mValue()
+	{
+		std::cout << " + [기본생성자]" << std::endl;
+	}
+	MyInt(int value) : mValue{ value }
+	{
+		std::cout << " + [생성자] : " << value << std::endl;
+	}
+	~MyInt()
+	{
+		std::cout << "-[소멸자 : " << mValue << std::endl;
+	}
+};
+
+MyInt operator + (MyInt op1, MyInt op2)
+{
+	return MyInt(op1.mValue + op2.mValue);
+}
+
+int main()
+{
+	Point2D pt1{ 2,3 };
+	Point2D pt2{ 3,4 };
+
+	Point2D pt3 = pt1 + pt2;
+
+	MyInt x(1), y(2);
+	
 }
